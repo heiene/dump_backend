7e0abfc4ac4ada5a605367d64cea1a670ac5a8f8
Øyvind Heiene <oyvindheiene@hotmail.com>
2016-05-28 23:52:20 +0200

    working backend

--- server.js
+++ server.js
@@ -1,81 +1,65 @@
 var express = require('express');
-var sqlite3 = require('sqlite3').verbose();
 var fs      = require('fs');
 var csv     = require('csv-parser');
 
-/*fs.createReadStream('./data/27.05.2016.csv')
-    .pipe(csv())
-    .on('data', function(data) {
-        console.log('row', data)
+var Datastore = require('nedb')
+    , db = new Datastore();
+
+var refreshData = function() {
+
+    console.log("Dropper database og kjører opp ny");
+
+    db.remove({}, { multi: true }, function (err, numRemoved) {
+        if(err) {
+            console.log('Error i refreshData', error);
+        } else {
+            console.log(numRemoved,' - Removed items');
+            loadData();
+        }
     });
-*/
-var restApi = express();
-/*
-restApi.get('/customer/:name', function(req,res){
-    db.all("SELECT order_ref as orderNumber, process_step as process FROM order WHERE customer_name = '" + req.params.name+ "'", function(err,row) {
-        if(err != null) {
-            //Error handling
-        } else {
-            console.log(row);
-            res.send(200, row)
-        }
-    })
-});
-
-restApi.get('/customer/:name/order/:id', function() {
-    db.all("SELECT order_ref as orderNumber, process_step as process FROM order WHERE order_ref = '" + req.params.id+ "'", function(err,row) {
-        if(err != null) {
-            //Error handling
-        } else {
-            console.log(row);
-            res.send(200, row)
-        }
-    })
-});
-*/
-
-var funki = function(){
-    console.log("funki created", db);
-
 };
 
-var handleData = function(){
-
-};
-
-var db = new sqlite3.Database(':memory:',sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, funki);
-
-db.serialize(function() {
-    console.log("creating table", db);
-    db.run("CREATE TABLE orders (" +
-        "order_ref TEXT, " +
-        "product_group TEXT, " +
-        "process_step TEXT, " +
-        "customer_name TEXT)");
-
-    console.log("creating table done", db);
-    //customer_name	order_ref	product_group	process_step	circuit_id	reg_date	plan_done_date	clean_order_date	agreed_date	agreed_installation_date	FM_date	finalize_access_date	latest_agreed_date	order_counter
-
-    fs.createReadStream('./data/27.05.2016.csv')
+var loadData = function() {
+    fs.createReadStream('./data/dump.csv')
         .pipe(csv())
         .on('data', function(data) {
-            db.run("INSERT into orders (customer_name ,order_ref, product_group, process_step) VALUES ('" +data.customer_name + "', '"+data.order_ref + "', '"+data.product_group + "', '"+data.process_step + "')");
-            //console.log('row', data)
+            db.insert(data);
         });
+};
 
-   /* var stmt = db.prepare("INSERT INTO orders VALUES (?)");
-    for (var i = 0; i < 10; i++) {
-        stmt.run("Ipsum " + i, "ejsy    q");
-    }
-    stmt.finalize();
+setInterval(refreshData, 5000);
 
-    db.each("SELECT order_ref as orderNumber, process_step as process FROM order", function(err, row) {
-        console.log(row.orderNumber + ":  : " + row.process);
-    });
-    */
+var restApi = express();
+
+restApi.get('/customer/:id', function(req,res){
+    db.find(
+        {customer_name: req.params.id},
+        function(err, docs){
+            res.status(200).send(docs);
+            console.log(docs);
+        })
 });
 
-db.close();
+restApi.get('/order/:id', function(req,res){
+    db.find(
+        {order_ref: req.params.id},
+        function(err, docs){
+            res.status(200).send(docs);
+            console.log(docs);
+        })
+});
+
+restApi.get('/product/:id', function(req,res){
+    db.find(
+        {product_group: req.params.id},
+        function(err, docs){
+            res.status(200).send(docs);
+            console.log(docs);
+        })
+});
 
 restApi.listen(3000);
+
 console.log('Server running at http://localhost:3000/');
+
+loadData();
